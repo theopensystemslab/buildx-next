f672cef557f28b7d58267ad8823f4ff233a28f40
Thomas O'Neill <tom@opensystemslab.io>
2022-02-18 12:44:38 +0000

    set type mock data

--- src/CONSTANTS.ts
+++ src/CONSTANTS.ts
@@ -13,3 +13,14 @@ export enum RaycasterLayer {
 export const BUILDX_LOCAL_STORAGE_HOUSES_KEY = "BUILDX_LOCAL_STORAGE_HOUSES_KEY"
 export const BUILDX_LOCAL_STORAGE_MAP_POLYGON_KEY =
   "BUILDX_LOCAL_STORAGE_MAP_POLYGON_KEY"
+
+type SetTypes = {
+  [key: string]: string[][]
+}
+
+export const setTypes: SetTypes = {
+  ST2: [
+    ["S1-MID-G1-ST2-L2-GRID2-SIDE0-SIDE0-SIDE0-SIDE0"],
+    ["S1-MID-M1-ST2-L2-GRID2-SIDE0-SIDE0-SIDE0-SIDE0"],
+  ],
+}
