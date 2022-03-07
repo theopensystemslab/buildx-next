6567a97b088705f54da1d2745546231227a25379
Thomas O'Neill <tom@opensystemslab.io>
2022-03-07 11:56:35 +0000

    findA2

--- src/utils/fp/index.ts
+++ src/utils/fp/index.ts
@@ -1,41 +1,39 @@
-import { flow, pipe } from "fp-ts/lib/function"
+import { flow } from "fp-ts/lib/function"
 import { concatAll } from "fp-ts/lib/Monoid"
 import { map as mapO } from "fp-ts/lib/Option"
 import { modifyAt } from "fp-ts/lib/ReadonlyArray"
 import { Monoid, split, toUpperCase } from "fp-ts/lib/string"
 
-export { mapO }
-
 export {
-  map as mapRR,
-  filter as filterRR,
-  reduce as reduceRR,
-  filterMap as filterMapRR,
-  filterMapWithIndex as filterMapWithIndexRR,
-} from "fp-ts/lib/ReadonlyRecord"
-export {
-  map as mapR,
-  filter as filterR,
-  reduce as reduceR,
-} from "fp-ts/lib/Record"
-export {
-  map as mapRA,
-  mapWithIndex as mapWithIndexRA,
-  filter as filterRA,
-  reduce as reduceRA,
-  chunksOf as chunksOfRA,
-} from "fp-ts/lib/ReadonlyArray"
-export {
-  map as mapA,
   filter as filterA,
+  map as mapA,
   reduce as reduceA,
 } from "fp-ts/lib/Array"
+export {
+  chunksOf as chunksOfRA,
+  filter as filterRA,
+  map as mapRA,
+  mapWithIndex as mapWithIndexRA,
+  reduce as reduceRA,
+} from "fp-ts/lib/ReadonlyArray"
 export {
   chunksOf as chunksOfRNA,
   mapWithIndex as mapWithIndexRNA,
 } from "fp-ts/lib/ReadonlyNonEmptyArray"
-
+export {
+  filter as filterRR,
+  filterMap as filterMapRR,
+  filterMapWithIndex as filterMapWithIndexRR,
+  map as mapRR,
+  reduce as reduceRR,
+} from "fp-ts/lib/ReadonlyRecord"
+export {
+  filter as filterR,
+  map as mapR,
+  reduce as reduceR,
+} from "fp-ts/lib/Record"
 export { map as mapT } from "fp-ts/lib/Task"
+export { mapO }
 
 export const pipeLog = <T extends unknown>(x: T): T => (console.log(x), x)
 
@@ -50,3 +48,28 @@ export const any = (...args: boolean[]) =>
 export const all = (...args: boolean[]) =>
   args.reduce((acc, v) => acc && v, true)
 // export const all = concatAll(MonoidAll)
+
+export const findA2 =
+  <T extends unknown>(target: T[][]) =>
+  (input: T[][]): [number, number][] => {
+    let indices: [number, number][] = []
+    for (let x = 0; x < input.length; x++) {
+      for (let y = 0; y < input[x].length; y++) {
+        if (
+          (function () {
+            for (let tx = 0; tx < target.length; tx++) {
+              for (let ty = 0; ty < target[tx].length; ty++) {
+                if (target[tx][ty] !== input?.[x + tx]?.[y + ty]) {
+                  return false
+                }
+              }
+            }
+            return true
+          })()
+        ) {
+          indices = [...indices, [x, y]]
+        }
+      }
+    }
+    return indices
+  }
