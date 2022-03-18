1cffdc69ab9b4ebd287998ddc38bde1c1a5daa12
Thomas O'Neill <tom@opensystemslab.io>
2022-03-18 11:10:18 +0000

    module debug traverse groups

--- src/components/debug/ModuleDebug.tsx
+++ src/components/debug/ModuleDebug.tsx
@@ -1,17 +1,10 @@
 import { HouseType } from "@/data/houseType"
 import { Module } from "@/data/module"
 import { useDebug } from "@/stores/debug"
-import { useSystemsData } from "@/stores/systems"
-import { filterRA, mapRA, reduceRA } from "@/utils"
-import { map } from "fp-ts/lib/Array"
+import { reduceRA } from "@/utils"
 import { pipe } from "fp-ts/lib/function"
 import { none, some } from "fp-ts/lib/Option"
-import {
-  filterMap,
-  filterMapWithIndex,
-  findFirst,
-  flatten,
-} from "fp-ts/lib/ReadonlyArray"
+import { filterMapWithIndex } from "fp-ts/lib/ReadonlyArray"
 import { useControls } from "leva"
 import React, { Fragment, Suspense, useEffect, useState } from "react"
 import ModuleDebugModule from "./ModuleDebugModule"
--- src/components/debug/ModuleDebugModule.tsx
+++ src/components/debug/ModuleDebugModule.tsx
@@ -1,36 +1,35 @@
 import { Module } from "@/data/module"
 import defaultMaterial from "@/materials/defaultMaterial"
-import { isMesh, mapRA, useGLTF } from "@/utils"
+import { isMesh, pipeLog, useGLTF } from "@/utils"
+import { values } from "fp-ts-std/ReadonlyRecord"
 import { pipe } from "fp-ts/lib/function"
+import { mapWithIndex, reduce } from "fp-ts/lib/ReadonlyArray"
 import { toReadonlyArray } from "fp-ts/lib/ReadonlyRecord"
+import produce from "immer"
+import { Mesh } from "three"
 
 const ModuleDebugModule = ({ module }: { module: Module }) => {
   const gltf = useGLTF(module.modelUrl)
 
   const meshes = pipe(
     gltf.nodes,
-    toReadonlyArray,
-    mapRA(([, node]) => {
-      if (isMesh(node)) {
-        return (
-          <mesh
-            key={node.id}
-            material={defaultMaterial}
-            geometry={node.geometry}
-          ></mesh>
-        )
-      } else {
-        console.log(node)
-        return null
-      }
-    })
+    values,
+    reduce([], (acc: Mesh[], node) => {
+      return produce(acc, (draft) => {
+        node.traverse((child) => {
+          if (isMesh(child)) {
+            draft.push(child)
+          }
+        })
+      })
+    }),
+    pipeLog,
+    mapWithIndex((i, node) => (
+      <mesh key={i} material={defaultMaterial} geometry={node.geometry} />
+    ))
   )
 
   return <group>{meshes}</group>
-  // map((meshes) => mergeBufferGeometries(meshes.map((mesh) => mesh.geometry))),
-  // filter((bg: BufferGeometry | null): bg is BufferGeometry => Boolean(bg)),
-  // filterWithIndex((k) => k !== "Appliance"), // model needs clean-up??
-  // mapWithIndex((elementName, geometry) => (
 }
 
 export default ModuleDebugModule
