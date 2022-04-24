5eb6039d5fc36631a8dd079fa31c4c3768c08184
Peter Szerzo <szerzo.peter@gmail.com>
2022-04-24 18:45:17 +0200

    Separate out components

--- src/components/dashboard/Dashboard.tsx
+++ src/components/dashboard/Dashboard.tsx
@@ -3,133 +3,32 @@ import { useHouses } from "@/stores/houses"
 import { Close } from "@/components/ui/icons"
 import React, { useMemo, useState, type FC } from "react"
 import Link from "next/link"
-import { nativeEnum } from "zod"
+import BasicChart from "./BasicChart"
+import DataPoint from "./DataPoint"
+import { type Module } from "@/data/module"
+import { Tab, tabs, fromSlug } from "./dashboardTabs"
 
 export interface Props {
   slug?: string
-}
-
-export enum Tab {
-  Overview = "overview",
-  BuildingAreas = "building-areas",
-  BuildCosts = "build-costs",
-  EnergyUse = "energy-use",
-  OperationalCo2 = "operational-co2",
-  EmbodiedCo2 = "embodied-co2",
-}
-
-const TabEnum = nativeEnum(Tab)
-
-const tabs: { id: Tab; label: string }[] = [
-  { id: Tab.Overview, label: "Overview" },
-  { id: Tab.BuildingAreas, label: "Building Areas" },
-  { id: Tab.BuildCosts, label: "Build Costs" },
-  { id: Tab.EnergyUse, label: "Energy Use" },
-  { id: Tab.OperationalCo2, label: "Operational CO₂" },
-  { id: Tab.EmbodiedCo2, label: "Embodied CO₂" },
-]
-
-const fromSlug = (slug: string | undefined): Tab => {
-  const parsed = TabEnum.safeParse(slug)
-  if (parsed.success) {
-    return parsed.data
-  }
-  return Tab.Overview
-}
-
-const SingleDataPoint: FC<{
-  label: string
-  value: number
-  unitOfMeasurement: string
-  explanation: string
-}> = (props) => (
-  <div className="space-y-4">
-    <p className="text-sm">{props.label}</p>
-    <div>
-      <p className="text-4xl text-gray-400">{`${props.value}${props.unitOfMeasurement}`}</p>
-      <p className="text-sm text-gray-400">{props.explanation}</p>
-    </div>
-  </div>
-)
-
-const BasicChart: FC<{
-  label: string
-  data: number[]
-  explanation: string
-}> = (props) => {
-  const max = useMemo(() => Math.max(...props.data), [props.data])
-  const min = useMemo(() => Math.min(...props.data), [props.data])
-
-  const w = 60
-  const h = 100
-
-  const baseline = min < 0 ? h / 2 : h
-
-  const scale = (min < 0 ? h / 2 : h) * 0.85
-
-  return (
-    <div className="space-y-4">
-      <p className="text-sm">{props.label}</p>
-      <div className="space-y-2">
-        <svg width="200" viewBox={`0 0 ${w} ${h}`}>
-          {props.data.map((point, index) => {
-            const width = (w / (props.data.length + 1)) * 0.6
-            const height = (point * scale) / max
-            const x = (w * (index + 1)) / (props.data.length + 1) - width / 2
-            return (
-              <>
-                <rect
-                  x={x}
-                  y={baseline - Math.max(height, 0)}
-                  width={width}
-                  height={Math.abs(height)}
-                  stroke="none"
-                  fill={index === 0 ? "#3EFF80" : "#9D9D9D"}
-                />
-                <text
-                  x={x + width / 2}
-                  y={baseline - height - (height < 0 ? -6 : 3)}
-                  fill="#9D9D9D"
-                  textAnchor="middle"
-                  style={{ fontSize: 4 }}
-                >
-                  {point}
-                </text>
-              </>
-            )
-          })}
-          <line
-            x1="0"
-            y1={baseline - 0.25}
-            x2={w}
-            y2={baseline - 0.25}
-            stroke="#000"
-            strokeWidth="0.5"
-          />
-        </svg>
-        <p className="text-sm text-gray-400">{props.explanation}</p>
-      </div>
-    </div>
-  )
 }
 
 const Overview = () => {
   return (
     <div className="space-y-16">
       <div className="grid grid-cols-4 space-x-4">
-        <SingleDataPoint
+        <DataPoint
           label="Total site area"
           value={350}
           unitOfMeasurement="m²"
           explanation="gross internal area"
         />
-        <SingleDataPoint
+        <DataPoint
           label="Total building area"
           value={350}
           unitOfMeasurement="m²"
           explanation="gross internal area"
         />
-        <SingleDataPoint
+        <DataPoint
           label="Number of units"
           value={2}
           unitOfMeasurement=""
@@ -138,17 +37,22 @@ const Overview = () => {
       </div>
       <div className="grid grid-cols-4 space-x-4">
         <BasicChart
-          label="Total site area"
+          label="Estimated total building cost"
           data={[10, 20]}
           explanation="gross internal area"
         />
         <BasicChart
-          label="Total building area"
+          label="Annual energy demand"
           data={[-2, 8]}
           explanation="gross internal area"
         />
         <BasicChart
-          label="Number of units"
+          label="Total operational CO₂"
+          data={[4, 1]}
+          explanation="gross internal area"
+        />
+        <BasicChart
+          label="Total embodied CO₂"
           data={[4, 1]}
           explanation="gross internal area"
         />
@@ -162,9 +66,31 @@ const Dashboard: FC<Props> = (props) => {
   const systemsData = useSystemsData()
   const activeTab = fromSlug(props.slug)
 
-  const [selectedBuildings, setSelectedBuildings] = useState<string[]>(
+  const [selectedHouses, setSelectedHouses] = useState<string[]>(
     Object.keys(houses).slice(0, 1)
   )
+
+  // TODO: extract into @/stores/houses.ts once finalized
+  const modulesByHouse = useMemo(() => {
+    const obj: Record<string, Module[]> = {}
+    if (!systemsData || systemsData === "error") {
+      return
+    }
+    selectedHouses.forEach((houseId) => {
+      const house = houses[houseId]
+      if (!house) {
+        return
+      }
+      obj[houseId] = house.dna
+        .map((dna) =>
+          systemsData.modules.find(
+            (module) => module.dna === dna && module.systemId === house.systemId
+          )
+        )
+        .filter((module): module is Module => Boolean(module))
+    })
+    return obj
+  }, [systemsData, selectedHouses])
 
   return (
     <div className="w-full h-full overflow-auto bg-gray-100">
@@ -183,21 +109,17 @@ const Dashboard: FC<Props> = (props) => {
               if (!houseId) {
                 return null
               }
-              setSelectedBuildings((prev) => [...prev, houseId])
+              setSelectedHouses((prev) => [...prev, houseId])
             }}
           />
           <datalist id="buildings">
             {Object.entries(houses).map(([houseId, house]) =>
-              selectedBuildings.includes(houseId) ? null : (
-                <option
-                  key={houseId}
-                  value={house.friendlyName}
-                  data-houseId={houseId}
-                />
+              selectedHouses.includes(houseId) ? null : (
+                <option key={houseId} value={house.friendlyName} />
               )
             )}
           </datalist>
-          {selectedBuildings.map((houseId) => {
+          {selectedHouses.map((houseId) => {
             const house = houses[houseId]
             if (!house) {
               return null
@@ -213,7 +135,7 @@ const Dashboard: FC<Props> = (props) => {
                 <button
                   className="h-8 w-8 p-0.5 hover:bg-gray-50"
                   onClick={() => {
-                    setSelectedBuildings((prev) =>
+                    setSelectedHouses((prev) =>
                       prev.filter((id) => id !== houseId)
                     )
                   }}
