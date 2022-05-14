ee42a611ca71df776afe16d3e1bb3c4950c1091e
Peter Szerzo <szerzo.peter@gmail.com>
2022-05-14 09:14:35 +0200

    Expand energy use calculations

--- src/components/dashboard/data.ts
+++ src/components/dashboard/data.ts
@@ -1,7 +1,8 @@
 import { type Module } from "@/data/module"
 import { type SystemsData } from "@/data/system"
-import { type Houses } from "@/data/house"
+import { type House, type Houses } from "@/data/house"
 import { type EnergyInfo } from "@/data/energyInfo"
+import { type SpaceType } from "@/data/spaceType"
 
 export interface DashboardData {
   byHouse: Record<string, HouseInfo>
@@ -10,37 +11,59 @@ export interface DashboardData {
   costs: Costs
   operationalCo2: OperationalCo2
   embodiedCo2: EmbodiedCo2
+  energyUse: EnergyUse
 }
 
 // Areas
 
 export interface Areas {
-  building: number
   foundation: number
   groundFloor: number
   firstFloor: number
   secondFloor: number
-  roof: number
+  cladding: number
+  internalLining: number
+  roofing: number
+  bedroom: number
+  bathroom: number
+  living: number
+  kitchen: number
+  stairs: number
+  windowsAndDoors: number
 }
 
 const emptyAreas = (): Areas => ({
-  building: 0,
   foundation: 0,
   groundFloor: 0,
   firstFloor: 0,
   secondFloor: 0,
-  roof: 0,
+  cladding: 0,
+  internalLining: 0,
+  roofing: 0,
+  bedroom: 0,
+  bathroom: 0,
+  living: 0,
+  kitchen: 0,
+  stairs: 0,
+  windowsAndDoors: 0,
 })
 
 const accumulateAreas = (areas: Areas[]): Areas =>
   areas.reduce((accumulator, current) => {
     return {
-      building: accumulator.building + current.building,
       foundation: accumulator.foundation + current.foundation,
       groundFloor: accumulator.groundFloor + current.groundFloor,
       firstFloor: accumulator.firstFloor + current.firstFloor,
       secondFloor: accumulator.secondFloor + current.secondFloor,
-      roof: accumulator.roof + current.roof,
+      cladding: accumulator.cladding + current.cladding,
+      internalLining: accumulator.roofing + current.internalLining,
+      roofing: accumulator.roofing + current.roofing,
+      bedroom: accumulator.bedroom + current.bedroom,
+      bathroom: accumulator.bathroom + current.bathroom,
+      living: accumulator.living + current.living,
+      kitchen: accumulator.kitchen + current.kitchen,
+      stairs: accumulator.stairs + current.stairs,
+      windowsAndDoors: accumulator.windowsAndDoors + current.windowsAndDoors,
     }
   }, emptyAreas())
 
@@ -110,6 +133,7 @@ export interface EmbodiedCo2 {
   modules: number
   cladding: number
   total: number
+  comparative: number
 }
 
 const emptyEmbodiedCo2 = (): EmbodiedCo2 => ({
@@ -117,6 +141,7 @@ const emptyEmbodiedCo2 = (): EmbodiedCo2 => ({
   modules: 0,
   cladding: 0,
   total: 0,
+  comparative: 0,
 })
 
 const accumulateEmbodiedCo2 = (values: EmbodiedCo2[]): EmbodiedCo2 =>
@@ -126,8 +151,51 @@ const accumulateEmbodiedCo2 = (values: EmbodiedCo2[]): EmbodiedCo2 =>
       modules: accumulator.modules + current.modules,
       cladding: accumulator.cladding + current.cladding,
       total: accumulator.total + current.total,
+      comparative: accumulator.comparative + current.comparative,
     }
   }, emptyEmbodiedCo2())
+
+// Energy use
+
+export interface EnergyUse {
+  dhwDemand: number
+  spaceHeatingDemand: number
+  totalHeatingDemand: number
+  primaryEnergyDemand: number
+  dhwCost: number
+  spaceHeatingCost: number
+  totalHeatingCost: number
+  primaryEnergyCost: number
+}
+
+const emptyEnergyUse = (): EnergyUse => ({
+  dhwDemand: 0,
+  spaceHeatingDemand: 0,
+  totalHeatingDemand: 0,
+  primaryEnergyDemand: 0,
+  dhwCost: 0,
+  spaceHeatingCost: 0,
+  totalHeatingCost: 0,
+  primaryEnergyCost: 0,
+})
+
+const accumulateEnergyUse = (values: EnergyUse[]): EnergyUse =>
+  values.reduce((accumulator, current) => {
+    return {
+      dhwDemand: accumulator.dhwDemand + current.dhwDemand,
+      spaceHeatingDemand:
+        accumulator.spaceHeatingDemand + current.spaceHeatingDemand,
+      totalHeatingDemand:
+        accumulator.totalHeatingDemand + current.totalHeatingDemand,
+      primaryEnergyDemand:
+        accumulator.primaryEnergyDemand + current.primaryEnergyDemand,
+      dhwCost: accumulator.dhwCost + current.dhwCost,
+      spaceHeatingCost: accumulator.spaceHeatingCost + current.spaceHeatingCost,
+      totalHeatingCost: accumulator.totalHeatingCost + current.totalHeatingCost,
+      primaryEnergyCost:
+        accumulator.primaryEnergyCost + current.primaryEnergyCost,
+    }
+  }, emptyEnergyUse())
 
 export interface HouseInfo {
   houseModules: Module[]
@@ -135,13 +203,25 @@ export interface HouseInfo {
   costs: Costs
   operationalCo2: OperationalCo2
   embodiedCo2: EmbodiedCo2
+  energyUse: EnergyUse
   cost: number
   embodiedCarbon: number
 }
 
+// TODO: retrieve from Airtable instead of hard-coding
+const comparative = {
+  cost: 1600,
+  operationalCo2: 20,
+  embodiedCo2: 300,
+  energyUse: 120,
+  electricityTariff: 0.2,
+}
+
 const calculateHouseInfo = (
+  house: House,
   houseModules: Module[],
-  energyInfo: EnergyInfo
+  energyInfo: EnergyInfo,
+  spaceTypes: SpaceType[]
 ): HouseInfo => {
   const accumulateIf = (
     fn: (module: Module) => boolean,
@@ -152,35 +232,89 @@ const calculateHouseInfo = (
     }, 0)
   }
 
-  const areas = {
-    building: accumulateIf(
-      (module) => module.structuredDna.levelType[0] === "G",
-      (module) => module.width * module.height
-    ),
+  const bedroomId = spaceTypes.find(
+    (spaceType) =>
+      spaceType.systemId === house.systemId && spaceType.code === "BEDR"
+  )?.id
+
+  const bathroomId = spaceTypes.find(
+    (spaceType) =>
+      spaceType.systemId === house.systemId && spaceType.code === "BATH"
+  )?.id
+
+  const livingId = spaceTypes.find(
+    (spaceType) =>
+      spaceType.systemId === house.systemId && spaceType.code === "LIVN"
+  )?.id
+
+  const kitchenId = spaceTypes.find(
+    (spaceType) =>
+      spaceType.systemId === house.systemId && spaceType.code === "KITC"
+  )?.id
+
+  const totalFloorArea = accumulateIf(
+    () => true,
+    (module) => module.floorArea
+  )
+
+  const areas: Areas = {
     foundation: accumulateIf(
       (module) => module.structuredDna.levelType[0] === "F",
-      (module) => module.width * module.height
+      (module) => module.floorArea
     ),
     groundFloor: accumulateIf(
       (module) => module.structuredDna.levelType[0] === "G",
-      (module) => module.width * module.height
+      (module) => module.floorArea
     ),
     firstFloor: accumulateIf(
       (module) => module.structuredDna.levelType[0] === "M",
-      (module) => module.width * module.height
+      (module) => module.floorArea
     ),
     secondFloor: accumulateIf(
       (module) => module.structuredDna.levelType[0] === "T",
-      (module) => module.width * module.height
+      (module) => module.floorArea
     ),
-    roof: accumulateIf(
-      (module) => module.structuredDna.levelType[0] === "R",
-      (module) => module.width * module.height
+    internalLining: accumulateIf(
+      () => true,
+      (module) => module.liningArea
+    ),
+    cladding: accumulateIf(
+      () => true,
+      (module) => module.claddingArea
+    ),
+    roofing: accumulateIf(
+      () => true,
+      (module) => module.roofingArea
+    ),
+    bedroom: accumulateIf(
+      (module) => module.spaceType === bedroomId,
+      (module) => module.floorArea
+    ),
+    bathroom: accumulateIf(
+      (module) => module.spaceType === bathroomId,
+      (module) => module.floorArea
+    ),
+    living: accumulateIf(
+      (module) => module.spaceType === livingId,
+      (module) => module.floorArea
+    ),
+    kitchen: accumulateIf(
+      (module) => module.spaceType === kitchenId,
+      (module) => module.floorArea
+    ),
+    // TODO: calculate
+    stairs: accumulateIf(
+      () => true,
+      (module) => module.floorArea
+    ),
+    // TODO: calculate
+    windowsAndDoors: accumulateIf(
+      () => true,
+      (module) => module.floorArea
     ),
   }
 
-  // TODO: finish calculation
-  const costs = {
+  const costs: Costs = {
     foundation: accumulateIf(
       (module) => module.structuredDna.levelType[0] === "F",
       (module) => module.cost
@@ -190,34 +324,33 @@ const calculateHouseInfo = (
       (module) => module.cost
     ),
     superstructure: accumulateIf(
-      (module) => module.structuredDna.levelType[0] === "R",
+      (module) =>
+        module.structuredDna.levelType[0] !== "F" &&
+        module.structuredDna.levelType[0] !== "R",
       (module) => module.cost
     ),
+    // TODO: calculate
     roofCovering: 0,
+    // TODO: calculate
     internalLining: 0,
+    // TODO: calculate
     cladding: 0,
     total: accumulateIf(
       () => true,
+      // TODO: add roof, lining and cladding
       (module) => module.cost
     ),
   }
 
-  const annualTotalOperationalCo2 = accumulateIf(
-    (module) => true,
-    (module) => module.width * module.height * energyInfo.operationalCo2
-  )
+  const annualTotalOperationalCo2 = totalFloorArea * energyInfo.operationalCo2
 
-  const operationalCo2 = {
+  const operationalCo2: OperationalCo2 = {
     annualTotal: annualTotalOperationalCo2,
-    annualComparative: accumulateIf(
-      (module) => true,
-      // TODO: figure out calculation
-      (module) => module.width * module.height * 30
-    ),
+    annualComparative: totalFloorArea * comparative.operationalCo2,
     lifetime: annualTotalOperationalCo2 * 60,
   }
 
-  const embodiedCo2 = {
+  const embodiedCo2: EmbodiedCo2 = {
     foundations: accumulateIf(
       (module) => module.structuredDna.levelType[0] === "F",
       (module) => module.embodiedCarbon
@@ -227,13 +360,26 @@ const calculateHouseInfo = (
       (module) => module.embodiedCarbon
     ),
     cladding: accumulateIf(
-      (module) => module.structuredDna.levelType[0] !== "R",
+      (module) => module.structuredDna.levelType[0] === "R",
       (module) => module.embodiedCarbon
     ),
+    comparative: totalFloorArea * comparative.embodiedCo2,
     total: accumulateIf(
       () => true,
       (module) => module.embodiedCarbon
     ),
+  }
+
+  const energyUse: EnergyUse = {
+    dhwDemand: totalFloorArea * energyInfo.dhwDemand,
+    spaceHeatingDemand: totalFloorArea * energyInfo.spaceHeatingDemand,
+    totalHeatingDemand: totalFloorArea * energyInfo.totalHeatingDemand,
+    primaryEnergyDemand: totalFloorArea * energyInfo.primaryEnergyDemand,
+    dhwCost:
+      totalFloorArea * energyInfo.dhwDemand * energyInfo.electricityTariff,
+    spaceHeatingCost: totalFloorArea * energyInfo.spaceHeatingDemand * energyInfo.electricityTariff,
+    totalHeatingCost: totalFloorArea * energyInfo.totalHeatingDemand * energyInfo.electricityTariff,
+    primaryEnergyCost: totalFloorArea * energyInfo.primaryEnergyDemand * energyInfo.electricityTariff,
   }
 
   return {
@@ -246,6 +392,7 @@ const calculateHouseInfo = (
       () => true,
       (module) => module.cost
     ),
+    energyUse,
     embodiedCarbon: accumulateIf(
       () => true,
       (module) => module.embodiedCarbon
@@ -262,8 +409,6 @@ const calculate = ({
   systemsData: SystemsData
   selectedHouses: string[]
 }): DashboardData => {
-  console.log(systemsData)
-
   const byHouse = (() => {
     const obj: Record<string, HouseInfo> = {}
 
@@ -289,7 +434,12 @@ const calculate = ({
         )
         .filter((module): module is Module => Boolean(module))
 
-      obj[houseId] = calculateHouseInfo(modules, energyInfo)
+      obj[houseId] = calculateHouseInfo(
+        house,
+        modules,
+        energyInfo,
+        systemsData.spaceTypes
+      )
     })
     return obj
   })()
@@ -307,6 +457,9 @@ const calculate = ({
     ),
     embodiedCo2: accumulateEmbodiedCo2(
       Object.values(byHouse).map((houseInfo) => houseInfo.embodiedCo2)
+    ),
+    energyUse: accumulateEnergyUse(
+      Object.values(byHouse).map((houseInfo) => houseInfo.energyUse)
     ),
     unitsCount: selectedHouses.length,
   }
