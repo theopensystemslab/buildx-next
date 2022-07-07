ceed3155270a7a1909245a6c20fa779e84694d23
Thomas O'Neill <tom@opensystemslab.io>
2022-07-07 12:30:29 +0100

    mvt

--- src/components/site/MapTiles.tsx
+++ src/components/site/MapTiles.tsx
@@ -4,20 +4,31 @@ import mapProxy, {
   metersPerPixel,
   toLonLatPolygon,
 } from "@/stores/map"
-import { mapO } from "@/utils"
+import { flattenA, mapA, mapO, pipeLog, pipeLogWith } from "@/utils"
 import geojsonArea from "@mapbox/geojson-area"
 import * as cover from "@mapbox/tile-cover"
+import { Extrude } from "@react-three/drei"
 import { useLoader } from "@react-three/fiber"
+import { toMercator } from "@turf/turf"
 import { pipe } from "fp-ts/lib/function"
 import { fromNullable, getOrElse } from "fp-ts/lib/Option"
+import { Feature, Polygon, Position } from "geojson"
+import produce from "immer"
+import { fromLonLat, toLonLat } from "ol/proj"
+import { useEffect, useMemo } from "react"
+import { Shape } from "three"
 import { useSnapshot } from "valtio"
+import * as tilebelt from "@mapbox/tilebelt"
+
+const midPipe = (feature: Feature<Polygon>) =>
+  JSON.stringify(feature.geometry.coordinates, null, 2)
+
+const log = pipeLogWith(midPipe)
 
 const tileToURL = ([x, y, z]: [number, number, number]) => {
-  const url = `https://api.mapbox.com/v4/mapbox.mapbox-streets-v8/${z}/${x}/${y}.mvt?access_token=${process.env.NEXT_PUBLIC_MAPBOX_ACCESS_TOKEN}`
-  // console.log(url)
+  const url = `https://api.mapbox.com/v4/mapbox.mapbox-streets-v8/${z}/${x}/${y}.mvt?style=mapbox://styles/mapbox/light-v10@00&access_token=${process.env.NEXT_PUBLIC_MAPBOX_ACCESS_TOKEN}`
   return url
 }
-
 // .then((blob) => URL.createObjectURL(blob))
 
 // const foo = "https://api.mapbox.com/v4/{tileset_id}/{zoom}/{x}/{y}.{format}"
@@ -28,19 +39,63 @@ type TileProps = {
   polygonCentre: [number, number]
 }
 
-const Tile = ({ tile, mpp, polygonCentre: centre }: TileProps) => {
-  const foo = useLoader(MVTLoader as any, tileToURL(tile))
+const extrudeSettings = { steps: 2, depth: 1, bevelEnabled: false }
 
-  console.log(foo)
-  // useEffect(() => {
-  //   ;(async () => {
-  //     const geometryData = await load(tileToURL(tile), MVTLoader)
+const Tile = ({ tile, mpp, polygonCentre }: TileProps) => {
+  const features: Feature[] = useLoader(
+    MVTLoader as any,
+    tileToURL(tile)
+  ) as any
 
-  //     console.log({ geometryData })
-  //   })()
-  // }, [])
+  console.log(JSON.stringify(features, null, 2))
 
   return null
+
+  const shape = useMemo(() => {
+    const _shape = new Shape()
+
+    const polygonFeatures = features.filter(
+      (f) => f.geometry.type === "Polygon"
+    ) as Feature<Polygon>[]
+
+    const realCoords = pipe(
+      polygonFeatures,
+      mapA((feature) =>
+        pipe(
+          feature,
+          toMercator,
+          produce((draft: Feature<Polygon>) => {
+            draft.geometry.coordinates[0].forEach((coords) => {
+              coords[0] -= polygonCentre[0]
+              coords[1] -= polygonCentre[1]
+            })
+          }),
+          ({ geometry: { coordinates } }): V2[] => coordinates[0] as V2[]
+        )
+      ),
+      flattenA
+    )
+
+    // console.log({
+    //   bbox: tilebelt.tileToBBOX(tile),
+    //   geojson: tilebelt.tileToGeoJSON(tile),
+    // })
+
+    _shape.moveTo(...realCoords[0])
+    for (const [x, y] of realCoords.slice(1)) {
+      _shape.lineTo(x, y)
+    }
+
+    return _shape
+  }, [features])
+
+  return (
+    <>
+      <Extrude args={[shape, extrudeSettings]} rotation={[Math.PI / 2, 0, 0]}>
+        <meshToonMaterial color="#3E64FF" />
+      </Extrude>
+    </>
+  )
 
   // return data ? (
   //   <TileWithData
@@ -85,12 +140,14 @@ const MapTiles = () => {
 
   return (
     <group position={[0, 0.05, 0]} rotation={[0, 0, 0]}>
-      {tiles.map((tile) => (
-        <Tile
-          key={JSON.stringify(tile)}
-          {...{ tile, mpp, polygonCentre: centre }}
-        />
-      ))}
+      {tiles
+        .map((tile) => (
+          <Tile
+            key={JSON.stringify(tile)}
+            {...{ tile, mpp, polygonCentre: centre }}
+          />
+        ))
+        .slice(2, 3)}
     </group>
   )
 }
--- src/loaders/mvt.ts
+++ src/loaders/mvt.ts
@@ -1,19 +1,60 @@
 import { load } from "@loaders.gl/core"
 import { MVTLoader as _MVTLoader } from "@loaders.gl/mvt"
+import { Feature } from "geojson"
 import { Loader, LoadingManager } from "three"
 
 class MVTLoader extends Loader {
-  constructor(manager: LoadingManager) {
+  constructor(manager?: LoadingManager) {
     super(manager)
   }
 
-  load(url: string, onLoad: (data: any) => void) {
-    let geometryData: any
-    ;(async () => {
-      geometryData = await load(url, _MVTLoader)
+  load(
+    url: string, // stringified [x,y,zoom]
+    onLoad?: (data: Feature[]) => void,
+    onProgress?: (event: ProgressEvent) => void,
+    onError?: (event: ErrorEvent) => void
+  ): Feature[] {
+    const features: Feature[] = []
 
-      onLoad(geometryData)
+    ;(async () => {
+      try {
+        const urlChunks = url.split("/")
+
+        const x = Number(urlChunks[6])
+        const y = Number(urlChunks[7].split(".")[0])
+        const z = Number(urlChunks[5])
+
+        const loaderOptions = {
+          mvt: {
+            coordinates: "wgs84",
+            tileIndex: {
+              x,
+              y,
+              z,
+            },
+          },
+        }
+
+        const geometryData = await load(url, _MVTLoader, loaderOptions)
+
+        geometryData.forEach((x: any) => void features.push(x))
+        onLoad?.(geometryData)
+      } catch (e) {
+        if (e instanceof ErrorEvent) {
+          onError?.(e)
+        }
+      }
+      return features
     })()
+
+    return []
+  }
+
+  loadAsync(
+    url: string,
+    onProgress?: (event: ProgressEvent) => void
+  ): Promise<Feature> {
+    return load(url, _MVTLoader)
   }
 }
 
